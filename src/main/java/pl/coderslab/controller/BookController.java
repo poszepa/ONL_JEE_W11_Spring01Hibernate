0745152fbbb313ec5c7a31bd2543e60485391683
Dominik Koszkul <koszkuld@ryanair.com>
2022-04-23 15:50:15 +0200

    Relacje w Hibernate

--- src/main/java/pl/coderslab/controller/BookController.java
+++ src/main/java/pl/coderslab/controller/BookController.java
@@ -1,22 +1,30 @@
 package pl.coderslab.controller;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
 import pl.coderslab.dao.BookDao;
+import pl.coderslab.dao.PublisherDao;
 import pl.coderslab.model.Book;
+import pl.coderslab.model.Publisher;
 
 @Controller
+@RequiredArgsConstructor
 public class BookController {
-    private BookDao bookDao;
-
-    public BookController(BookDao bookDao) {
-        this.bookDao = bookDao;
-    }
+    private final BookDao bookDao;
+    private final PublisherDao publisherDao;
 
     @GetMapping("add")
+    @ResponseBody
     public String addBook() {
+        Publisher publisher = new Publisher();
+        publisher.setName("Dominik");
+        publisherDao.save(publisher);
+
         Book book = new Book();
+        book.setPublisher(publisher);
         book.setTitle("Title 1");
         book.setDescription("Description 1");
         book.setRating(3);
@@ -31,7 +39,7 @@ public class BookController {
         return book.toString();
     }
 
-    @GetMapping("update/{id}/{title}")
+    @GetMapping(value = "update/{id}/{title}", produces = "text/plain")
     public String updateBook(@PathVariable long id, @PathVariable String title) {
         Book book = bookDao.findById(id);
         book.setTitle(title);
--- src/main/java/pl/coderslab/model/Book.java
+++ src/main/java/pl/coderslab/model/Book.java
@@ -1,12 +1,21 @@
 package pl.coderslab.model;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
+@Getter
+@Setter
+@ToString
 @Entity
 @Table(name = "books")
 public class Book {
@@ -22,45 +31,7 @@ public class Book {
 
     private String description;
 
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public Integer getRating() {
-        return rating;
-    }
-
-    public void setRating(Integer rating) {
-        this.rating = rating;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    @Override
-    public String toString() {
-        return "Book{" +
-                "id=" + id +
-                ", title='" + title + '\'' +
-                ", rating=" + rating +
-                ", description='" + description + '\'' +
-                '}';
-    }
+    @ManyToOne
+    @JoinColumn(name = "publisher_id")
+    private Publisher publisher;
 }
