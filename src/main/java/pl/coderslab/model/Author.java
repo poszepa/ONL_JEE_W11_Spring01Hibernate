1ae8b1706002f55da8ff9b8032e88db15e1e72bb
Kamil Poszepczyński <93219293+poszepa@users.noreply.github.com>
2022-05-07 11:09:33 +0200

    Add new pesel and email area.

--- src/main/java/pl/coderslab/model/Author.java
+++ src/main/java/pl/coderslab/model/Author.java
@@ -2,6 +2,7 @@ package pl.coderslab.model;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.validator.constraints.pl.PESEL;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -10,6 +11,8 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 import javax.persistence.Transient;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
 
 @Getter
 @Setter
@@ -21,11 +24,19 @@ public class Author {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotNull
     @Column(name = "first_name")
     private String firstName;
 
+    @NotNull
     @Column(name = "last_name")
     private String lastName;
+
+    @PESEL
+    private String pesel;
+
+    @Email
+    private String email;
 
     @Transient
     public String getFullName() {
