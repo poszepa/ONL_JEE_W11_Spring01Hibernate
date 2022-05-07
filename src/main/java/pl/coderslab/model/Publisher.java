4833ba71a104e57deb23ae1e99ff81a1177926b6
Kamil Poszepczyński <93219293+poszepa@users.noreply.github.com>
2022-05-07 11:14:28 +0200

    add new area regon and nip

--- src/main/java/pl/coderslab/model/Publisher.java
+++ src/main/java/pl/coderslab/model/Publisher.java
@@ -1,11 +1,15 @@
 package pl.coderslab.model;
 
+import org.hibernate.validator.constraints.pl.NIP;
+import org.hibernate.validator.constraints.pl.REGON;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
 
 @Entity
 @Table(name = "publishers")
@@ -15,8 +19,16 @@ public class Publisher {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotNull
     @Column
     private String name;
+
+    @NIP
+    private String nip;
+
+    @REGON
+    private String regon;
+
 
     public Long getId() {
         return id;
