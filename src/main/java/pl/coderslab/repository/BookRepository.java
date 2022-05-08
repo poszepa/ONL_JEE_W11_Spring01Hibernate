db93fdd4e9a2c943c515161f60b087d369bb6fbd
Kamil Poszepczyński <93219293+poszepa@users.noreply.github.com>
2022-05-08 09:44:51 +0200

    Practics with custom query with @Query annotation

--- src/main/java/pl/coderslab/repository/BookRepository.java
+++ src/main/java/pl/coderslab/repository/BookRepository.java
@@ -1,6 +1,8 @@
 package pl.coderslab.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import pl.coderslab.model.Author;
 import pl.coderslab.model.Book;
@@ -8,6 +10,7 @@ import pl.coderslab.model.Category;
 import pl.coderslab.model.Publisher;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface BookRepository extends JpaRepository<Book, Long> {
@@ -21,4 +24,18 @@ public interface BookRepository extends JpaRepository<Book, Long> {
     List<Book> findByRatingIsLessThan(int rating);
     Book findFirstByCategory(Category category);
 
+    @Query("SELECT b FROM Book b WHERE b.title = :title")
+    Optional<Book> customFindBookByTitle(@Param("title") String title);
+
+    @Query("SELECT b FROM Book b WHERE b.category = :category")
+    List<Book> customFindByCategory(@Param("category")Category category);
+
+    @Query("SELECT b FROM Book b WHERE b.rating BETWEEN :minValue AND :maxValue")
+    List<Book> customFindByRating(@Param("minValue")Integer minValue, @Param("maxValue")Integer maxValue);
+
+    @Query("SELECT b FROM Book b WHERE b.publisher = :publisher")
+    List<Book> customFindByPublisher(@Param("publisher")Publisher publisher);
+
+    @Query("SELECT b FROM Book b WHERE b.category = :category ORDER BY b.title ASC")
+    Optional<Book> customFindByCategorySortByTitle(@Param("category")Category category);
 }
