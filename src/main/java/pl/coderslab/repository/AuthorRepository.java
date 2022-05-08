6fd70805d6edef512f67ebcf530f1799e1180762
Kamil Poszepczyński <93219293+poszepa@users.noreply.github.com>
2022-05-08 09:52:49 +0200

    Practics with custom query with @Query annotation

--- src/main/java/pl/coderslab/repository/AuthorRepository.java
+++ src/main/java/pl/coderslab/repository/AuthorRepository.java
@@ -1,6 +1,8 @@
 package pl.coderslab.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import pl.coderslab.model.Author;
 
@@ -12,4 +14,10 @@ public interface AuthorRepository extends JpaRepository<Author, Long> {
     Author findByEmail(String email);
     Author findByPesel(String pesel);
     List<Author> findByLastName(String lastName);
+
+    @Query("SELECT a FROM Author a WHERE a.email LIKE :startWith%")
+    List<Author> findByEmailStartWith(@Param("startWith")String startWith);
+
+    @Query("SELECT a FROM Author a WHERE a.pesel LIKE :startWith%")
+    List<Author> findByPeselStartWith(@Param("startWith")String startWith);
 }
